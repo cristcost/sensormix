0eb06ea662523f3a1d276ed4a192f37f459e23d6
cristcost <cristiano.costantini@gmail.com>
2013-10-05 17:55:16 +0200

    added a camel project for developing the android application

---
+++ sensormix-mockep-app/src/main/java/com/google/developers/gdgfirenze/mockep/Launcher.java
@@ -0,0 +1,13 @@
+package com.google.developers.gdgfirenze.mockep;
+
+/**
+ * Hello world!
+ *
+ */
+public class Launcher 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
