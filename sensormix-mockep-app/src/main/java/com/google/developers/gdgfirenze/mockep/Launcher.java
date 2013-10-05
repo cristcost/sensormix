a0d5463aea04521b4d0510d96e5c84b8249a5d05
cristcost <cristiano.costantini@gmail.com>
2013-10-05 22:28:21 +0200

    added a tool for debugging http rest request from sensors

--- sensormix-mockep-app/src/main/java/com/google/developers/gdgfirenze/mockep/Launcher.java
+++ sensormix-mockep-app/src/main/java/com/google/developers/gdgfirenze/mockep/Launcher.java
@@ -1,13 +1,35 @@
 package com.google.developers.gdgfirenze.mockep;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
 /**
  * Hello world!
- *
+ * 
  */
-public class Launcher 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class Launcher {
+
+  public static void main(String[] args) throws Exception {
+    CamelContext context = new DefaultCamelContext();
+    context.addRoutes(new RouteBuilder() {
+      public void configure() {
+        from("jetty:http://0.0.0.0:8080/test")
+        .to("log:dump?showAll=true")
+        .choice().when(header(Exchange.HTTP_METHOD).in("POST","PUT"))
+        .to("file:target/incoming?fileName=msg-${date:now:yyyyMMdd_HHmmss_SSS}.js")
+        .end()
+        .setHeader("Content-Type", constant("application/json"))
+        .to("velocity:response_template.vm");
+      }
+    });
+
+    context.start();
+    System.out.println("Press ENTER to exit");
+    System.in.read();
+
+    System.out.println("exit");
+    context.stop();
+  }
 }
