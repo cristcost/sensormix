5cb74e98812e27597b27e3f0837b51ebcc504922
scigheri <stefanocigheri@gmail.com>
2013-10-17 20:15:55 +0200

    added package.info files both for model and service packages

---
+++ sensormix-datamodel-api/src/main/java/com/google/developers/gdgfirenze/service/package-info.java
@@ -0,0 +1,3 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://developers.google.com/gdgfirenze/ns/service",
+    elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.google.developers.gdgfirenze.service;
