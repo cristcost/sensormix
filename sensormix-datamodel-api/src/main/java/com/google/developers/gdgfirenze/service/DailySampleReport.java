0fb744f204726cd505e7fbc04fe01a7f93e473cf
scigheri <stefanocigheri@gmail.com>
2013-10-18 20:50:46 +0200

    annotated new classes

--- sensormix-datamodel-api/src/main/java/com/google/developers/gdgfirenze/service/DailySampleReport.java
+++ sensormix-datamodel-api/src/main/java/com/google/developers/gdgfirenze/service/DailySampleReport.java
@@ -2,7 +2,37 @@ package com.google.developers.gdgfirenze.service;
 
 import java.util.Date;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "DailySampleReport")
 public class DailySampleReport {
-  public long sampleCount;
-  public Date date;
+
+	@XmlAttribute(required = false, name = "sampleCount")
+	private long sampleCount;
+
+	@XmlAttribute(required = false, name = "date")
+	@XmlSchemaType(name = "dateTime")
+	private Date date;
+
+	public long getSampleCount() {
+		return sampleCount;
+	}
+
+	public void setSampleCount(long sampleCount) {
+		this.sampleCount = sampleCount;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
 }
--- sensormix-datamodel-api/src/main/java/com/google/developers/gdgfirenze/service/SampleReport.java
+++ sensormix-datamodel-api/src/main/java/com/google/developers/gdgfirenze/service/SampleReport.java
@@ -2,12 +2,44 @@ package com.google.developers.gdgfirenze.service;
 
 import java.util.List;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+
 public class SampleReport {
 
-  public String sensorId;
-  public String sampleType;
+	@XmlAttribute(required = true, name = "sensorId")
+	@XmlSchemaType(name = "anyURI")
+	private String sensorId;
 
-  // no wrapper
-  public List<DailySampleReport> dailySampleReports;
+	@XmlAttribute(required = false, name = "sampleType")
+	private String sampleType;
+
+	@XmlElement(required = false, name = "dailySampleReport")
+	private List<DailySampleReport> dailySampleReports;
+
+	public String getSensorId() {
+		return sensorId;
+	}
+
+	public void setSensorId(String sensorId) {
+		this.sensorId = sensorId;
+	}
+
+	public String getSampleType() {
+		return sampleType;
+	}
+
+	public void setSampleType(String sampleType) {
+		this.sampleType = sampleType;
+	}
+
+	public List<DailySampleReport> getDailySampleReports() {
+		return dailySampleReports;
+	}
+
+	public void setDailySampleReports(List<DailySampleReport> dailySampleReports) {
+		this.dailySampleReports = dailySampleReports;
+	}
 
 }
