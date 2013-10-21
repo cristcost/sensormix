be9941137547b1157a44d464e724f426f7e28304
cristcost <cristiano.costantini@gmail.com>
2013-10-21 10:11:46 +0200

    added skip of integration tests by default (enable them with mvn ... -Dintegration.tests)

---
+++ sensormix-dataservice-bundle/src/test/java/com/google/developers/gdgfirenze/dataservice/SensormixServiceJpaImplIntegrationTest.java
@@ -0,0 +1,44 @@
+package com.google.developers.gdgfirenze.dataservice;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.google.developers.gdgfirenze.model.Sensor;
+
+public class SensormixServiceJpaImplIntegrationTest {
+
+	private SensormixServiceJpaImpl sensormixServiceJpaImpl;
+	private EntityManagerFactory emf;
+
+	@Before
+	public void initialize() {
+		emf = Persistence
+				.createEntityManagerFactory("sensormix_db");
+		sensormixServiceJpaImpl = new SensormixServiceJpaImpl();
+		sensormixServiceJpaImpl.setEntityManagerFactory(emf);
+	}
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test1()  {
+		Sensor s=new Sensor();
+		s.setId("uri:giuseppe:android:77551144");
+		s.setName("Samsung Galaxy W");
+		s.setDescription("booooooo");
+		sensormixServiceJpaImpl.registerSensor(s);
+		List<String> ids = sensormixServiceJpaImpl.listSensorsIds();
+		assertEquals(1, ids.size());
+	}
+
+}
