80abd181fbef32bc27450ec09240ce7f6da4d22e
ggerla <giuseppe.gerla@gmail.com>
2013-11-04 03:31:46 -0800

    Optimized size for kryo serialization

--- sensormix-dataservice-bundle/src/main/java/com/google/developers/gdgfirenze/serializer/Serializer.java
+++ sensormix-dataservice-bundle/src/main/java/com/google/developers/gdgfirenze/serializer/Serializer.java
@@ -6,28 +6,32 @@ import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
 import com.google.developers.gdgfirenze.model.AbstractSample;
+import com.google.developers.gdgfirenze.model.NumericValueSample;
+import com.google.developers.gdgfirenze.model.PositionSample;
+import com.google.developers.gdgfirenze.model.WifiSignalSample;
 
 public class Serializer {
 	Kryo k;
-	
+
 	public Serializer() {
-		k=new Kryo();
+		k = new Kryo();
 		k.setClassLoader(Serializer.class.getClassLoader());
+		k.register(WifiSignalSample.class, 1000);
+		k.register(NumericValueSample.class, 1001);
+		k.register(PositionSample.class, 1002);
 	}
-	
+
 	public byte[] serialize(AbstractSample orig) {
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        Output buffer = new Output(bos);
-        k.writeClassAndObject(buffer, orig);
-        
-        byte[] retVal=new byte[buffer.total()];
-        System.arraycopy(buffer.getBuffer(), 0, retVal, 0, buffer.position());
-        return retVal;
-    }
-	
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		Output buffer = new Output(bos);
+		k.writeClassAndObject(buffer, orig);
+		buffer.close();
+		return bos.toByteArray();
+	}
+
 	public AbstractSample deserialize(byte[] buffer) {
-        Input iBuff = new Input(buffer);
-        
-        return (AbstractSample)k.readClassAndObject(iBuff);
-    }
+		Input iBuff = new Input(buffer);
+
+		return (AbstractSample) k.readClassAndObject(iBuff);
+	}
 }
