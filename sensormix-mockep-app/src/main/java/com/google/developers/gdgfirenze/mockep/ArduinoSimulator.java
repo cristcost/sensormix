b3a211d759ef4f974f04feba68749daa074c8b44
cristcost <cristiano.costantini@gmail.com>
2013-12-06 09:18:14 +0100

    fixed checkstyle on mockep project

--- sensormix-mockep-app/src/main/java/com/google/developers/gdgfirenze/mockep/ArduinoSimulator.java
+++ sensormix-mockep-app/src/main/java/com/google/developers/gdgfirenze/mockep/ArduinoSimulator.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2013, Cristiano Costantini, Giuseppe Gerla, Michele Ficarra, Sergio Ciampi, Stefano
+ * Cigheri.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.google.developers.gdgfirenze.mockep;
 
 import java.io.IOException;
@@ -8,31 +22,37 @@ import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.Random;
 
+/**
+ * The Class ArduinoSimulator.
+ * 
+ * It simulates the messages sent by the arduino sensormix project.
+ */
 public class ArduinoSimulator {
 
-	public static void main(String[] args) throws IOException,
-			InterruptedException {
+  public static void main(String[] args) throws IOException, InterruptedException {
 
-		Random rand = new Random();
-		sendData("{ 'sensor' : {'id':'urn:rixf:org.arduino/sensor_id', type : 'urn:rixf:net.sensormix/device_types/arduino', name: 'Arduino', description : 'Rev. 1.03, Temp and Light sensors'}}");
-		while (true) {
-			String data = "{ 'sample' : {'device_id':'urn:rixf:org.arduino/sensor_id', 'temp': '"
-					+ rand.nextDouble() + "', 'lux': '" + rand.nextDouble() + "'} }";
-			sendData(data);
-			Thread.sleep(10000);
-		}
-	}
+    Random rand = new Random();
+    sendData("{ 'sensor' : {'id':'urn:rixf:org.arduino/sensor_id', "
+        + "type : 'urn:rixf:net.sensormix/device_types/arduino', "
+        + "name: 'Arduino', description : 'Rev. 1.03, Temp and Light sensors'}}");
+    while (true) {
+      String data =
+          "{ 'sample' : {'device_id':'urn:rixf:org.arduino/sensor_id', 'temp': '"
+              + rand.nextDouble() + "', 'lux': '" + rand.nextDouble() + "'} }";
+      sendData(data);
+      Thread.sleep(10000);
+    }
+  }
 
-	private static void sendData(String data) throws SocketException,
-			UnknownHostException, IOException {
-		DatagramSocket clientSocket = new DatagramSocket();
-		InetAddress ipAddress = InetAddress.getByName("localhost");
+  private static void sendData(String data) throws SocketException, UnknownHostException,
+    IOException {
+    DatagramSocket clientSocket = new DatagramSocket();
+    InetAddress ipAddress = InetAddress.getByName("localhost");
 
-		byte[] sendData = data.getBytes();
-		DatagramPacket sendPacket = new DatagramPacket(sendData,
-				sendData.length, ipAddress, 10081);
-		clientSocket.send(sendPacket);
-		clientSocket.close();
-	}
+    byte[] sendData = data.getBytes();
+    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, 10081);
+    clientSocket.send(sendPacket);
+    clientSocket.close();
+  }
 
 }
--- sensormix-mockep-app/src/main/java/com/google/developers/gdgfirenze/mockep/Launcher.java
+++ sensormix-mockep-app/src/main/java/com/google/developers/gdgfirenze/mockep/Launcher.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2013, Cristiano Costantini, Giuseppe Gerla, Michele Ficarra, Sergio Ciampi, Stefano
+ * Cigheri.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.google.developers.gdgfirenze.mockep;
 
 import org.apache.camel.CamelContext;
@@ -6,8 +20,7 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
 /**
- * Hello world!
- * 
+ * The Class for Launching the test endpoints.
  */
 public class Launcher {
 
@@ -15,18 +28,14 @@ public class Launcher {
     CamelContext context = new DefaultCamelContext();
     context.addRoutes(new RouteBuilder() {
       public void configure() {
-        from("jetty:http://0.0.0.0:8080/test")
-        .to("log:dump?showAll=true")
-        .choice().when(header(Exchange.HTTP_METHOD).in("POST","PUT"))
-        .to("file:target/incoming?fileName=msg-${date:now:yyyyMMdd_HHmmss_SSS}.js")
-        .end()
-        .setHeader("Content-Type", constant("application/json"))
-        .to("velocity:response_template.vm");
-        
+        from("jetty:http://0.0.0.0:8080/test").to("log:dump?showAll=true").choice().when(
+            header(Exchange.HTTP_METHOD).in("POST", "PUT")).to(
+            "file:target/incoming?fileName=msg-${date:now:yyyyMMdd_HHmmss_SSS}.js").end().setHeader(
+            "Content-Type", constant("application/json")).to("velocity:response_template.vm");
+
         // Arduino route
-        from("mina2:udp://0.0.0.0:10081")
-        .to("log:dump?showAll=true")
-        .to("file:target/incoming?fileName=msg-${date:now:yyyyMMdd_HHmmss_SSS}.js");
+        from("mina2:udp://0.0.0.0:10081").to("log:dump?showAll=true").to(
+            "file:target/incoming?fileName=msg-${date:now:yyyyMMdd_HHmmss_SSS}.js");
       }
     });
 
