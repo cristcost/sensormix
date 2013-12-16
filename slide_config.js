50deafef1682f302d7abd56ec36ed6df8503c846
cristcost <cristiano.costantini@gmail.com>
2013-12-16 17:06:02 +0100

    official slides

---
+++ slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Sviluppare su OSGI<br/>con Camel e GWT',
+    subtitle: 'Aperitivo tecnologico del GDG Firenze<br/>http://www.gdg-firenze.info/',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [
+    { name: 'Cristiano Costantini' },
+    { name: 'Giuseppe Gerla' },
+    { name: 'Michele Ficarra' },
+    { name: 'Sergio Ciampi' },
+    { name: 'Stefano Cigheri' }]
+};
+
